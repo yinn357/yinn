1ac5136857aed4c1ec7770c37b60c23a2fb63332
Yinn <43907800@qq.com>
2022-09-27 15:21:58 +0800

    1.用户CRUD

--- yinn-modulars/yinn-module-system/src/main/java/top/yinn/modulars/system/controller/AdminUserController.java
+++ yinn-modulars/yinn-module-system/src/main/java/top/yinn/modulars/system/controller/AdminUserController.java
@@ -1,22 +1,22 @@
 package top.yinn.modulars.system.controller;
 
-import cn.dev33.satoken.annotation.SaCheckLogin;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.yinn.core.base.ApiResult;
 import top.yinn.core.base.PageParam;
 import top.yinn.core.base.PageResult;
 import top.yinn.core.constant.YinnConstant;
+import top.yinn.database.id.IdsDTO;
 import top.yinn.modulars.system.constant.SysConstant;
 import top.yinn.modulars.system.model.dto.UserDTO;
 import top.yinn.modulars.system.model.vo.UserVO;
 import top.yinn.modulars.system.service.UserService;
+
+import javax.validation.Valid;
 
 
 /**
@@ -25,7 +25,6 @@ import top.yinn.modulars.system.service.UserService;
  * @author Yinn
  */
 @RequiredArgsConstructor
-@SaCheckLogin
 @Slf4j
 @Api(value = "用户管理接口", tags = {"用户管理接口"})
 @RequestMapping(SysConstant.SYS_MODULE_CONTEXT_PATH + YinnConstant.Version.HTTP_API_VERSION_V1 + "/users")
@@ -39,11 +38,44 @@ public class AdminUserController {
 
     private final UserService userService;
 
-    @SaCheckLogin
     // @SaCheckPermission(value = PERMISSION_PREFIX + YinnConstant.Permission.RETRIEVE)
     @ApiOperation(value = "分页列表", produces = MediaType.APPLICATION_JSON_VALUE)
     @GetMapping
     public ApiResult<PageResult<UserVO>> list(PageParam pageParam, UserDTO dto) {
         return ApiResult.data(userService.list(pageParam, dto));
     }
+
+    // @SaCheckPermission(value = PERMISSION_PREFIX + YinnConstant.Permission.RETRIEVE)
+    @ApiOperation(value = "详情", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}")
+    public ApiResult<UserVO> getById(@PathVariable Long id) {
+        return ApiResult.data(userService.getOneById(id, true));
+    }
+
+    // @SaCheckPermission(value = PERMISSION_PREFIX + YinnConstant.Permission.CREATE)
+    @ApiOperation(value = "新增", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
+    public ApiResult<?> insert(@RequestBody @Valid UserDTO dto) {
+        dto.setId(null);
+        userService.saveOrUpdate(dto);
+        return ApiResult.success();
+    }
+
+    // @SaCheckPermission( value = PERMISSION_PREFIX + YinnConstant.Permission.UPDATE)
+    @ApiOperation(value = "编辑", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/{id}")
+    public ApiResult<?> update(@PathVariable Long id, @RequestBody @Valid UserDTO dto) {
+        dto.setId(id);
+        userService.saveOrUpdate(dto);
+        return ApiResult.success();
+    }
+
+    // @SaCheckPermission( value = PERMISSION_PREFIX + YinnConstant.Permission.DELETE)
+    @ApiOperation(value = "删除", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping
+    public ApiResult<?> delete(@RequestBody @Valid IdsDTO<Long> dto) {
+        userService.removeBatchByIds(dto.getIds());
+        return ApiResult.success();
+    }
+
 }
--- yinn-modulars/yinn-module-system/src/main/java/top/yinn/modulars/system/service/UserService.java
+++ yinn-modulars/yinn-module-system/src/main/java/top/yinn/modulars/system/service/UserService.java
@@ -1,10 +1,16 @@
 package top.yinn.modulars.system.service;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import top.yinn.core.base.PageParam;
 import top.yinn.core.base.PageResult;
+import top.yinn.core.exception.code.ExceptionCode;
+import top.yinn.core.utils.PwdUtil;
 import top.yinn.database.service.impl.BaseServiceImpl;
 import top.yinn.modulars.system.mapper.UserMapper;
 import top.yinn.modulars.system.model.dto.UserDTO;
@@ -24,10 +30,58 @@ public class UserService extends BaseServiceImpl<UserMapper, UserEntity> {
 
 
 	/**
-	 * 后台管理-分页列表
+	 * 后台用户管理-分页列表
 	 */
 	public PageResult<UserVO> list(PageParam pageParam, UserDTO dto) {
-
-		return null;
+		Page<UserEntity> entityPage = this.page(
+				new Page<>(pageParam.getPageNum(), pageParam.getPageSize()),
+				new LambdaQueryWrapper<UserEntity>()
+						.like(StrUtil.isNotEmpty(dto.getNickName()), UserEntity::getNickName, dto.getNickName())
+						.like(StrUtil.isNotEmpty(dto.getAccount()), UserEntity::getAccount, dto.getAccount())
+						.orderByDesc(UserEntity::getCreateTime)
+		);
+		return PageResult.convert(entityPage, UserVO.class);
 	}
+
+	/**
+	 * 根据 ID 取详情
+	 *
+	 * @param id 主键ID
+	 * @return VO Or null
+	 */
+	public UserVO getOneById(Long id) {
+		return this.getOneById(id, false);
+	}
+
+	/**
+	 * 根据 ID 取详情
+	 *
+	 * @param id      主键ID
+	 * @param checkId 是否在 ID 无效时抛出异常
+	 * @return VO Or 抛出无效Id异常
+	 */
+	public UserVO getOneById(Long id, boolean checkId) {
+		UserEntity userEntity = this.getById(id);
+		if (checkId) {
+			ExceptionCode.INVALID_ID.assertNotNull(userEntity);
+		}
+
+		return BeanUtil.toBean(userEntity, UserVO.class);
+	}
+
+	/**
+	 * 用户管理-新增或更新
+	 *
+	 * @param userDTO
+	 * @return 用户Id
+	 */
+	public Long saveOrUpdate(UserDTO userDTO) {
+		UserEntity userEntity = BeanUtil.toBean(userDTO, UserEntity.class);
+		// 用户密码加密
+		userEntity.setPassword(PwdUtil.encrypt(userEntity.getPassword()));
+		this.saveOrUpdate(userEntity);
+
+		return userEntity.getId();
+	}
+
 }
